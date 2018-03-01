e3c2302ff6f29470ce218a90709b42fd075b1c00
meatyminx <flexayoung@gmail.com>
2018-03-01 12:20:26 +0100

    first commit

--- api.js
+++ api.js
@@ -14,6 +14,38 @@ function catchErrors(fn) {
   return (req, res, next) => fn(req, res, next).catch(next);
 }
 
-/* todo útfæra api */
+async function fnReadAll(req, res) {
+  await readAll()
+    .then(data => res.status(200).json(data));
+}
+
+async function fnCreate(req, res) {
+  await create(req.body)
+    .then(data => res.status(data.status).json(data.data));
+}
+
+async function fnReadOne(req, res) {
+  await readOne(req.params.id)
+    .then(data => res.status(data.status).json(data.data));
+}
+
+async function fnUpdate(req, res) {
+  await update(req.params.id, req.body)
+    .then(data => res.status(data.status).json(data.data));
+}
+
+async function fnDelete(req, res) {
+  await del(req.params.id)
+    .then(data => res.status(data.status).json(data.data));
+}
+
+router.route('/')
+  .get(catchErrors(fnReadAll))
+  .post(catchErrors(fnCreate));
+
+router.route('/:id')
+  .get(catchErrors(fnReadOne))
+  .put(catchErrors(fnUpdate))
+  .delete(catchErrors(fnDelete));
 
 module.exports = router;
--- notes.js
+++ notes.js
@@ -1,6 +1,21 @@
-/* todo sækja pakka sem vantar  */
+const validator = require('validator');
+const xss = require('xss');
+const { Client } = require('pg');
 
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URL || 'postgres://:@localhost/meatyminx';
+
+
+function checkData(title, text, datetime) {
+  const errors = [];
+  if (typeof title !== 'string' || !validator.isLength(title, { min: 1, max: 255 })) {
+    errors.push({ field: 'title', error: 'title must be a non-empty string' });
+  } if (typeof text !== 'string') {
+    errors.push({ field: 'text', error: 'text must be a string' });
+  } if (!validator.isISO8601(datetime)) {
+    errors.push({ field: 'datetime', error: 'Datetime must be a ISO 8601 date' });
+  }
+  return errors;
+}
 
 /**
  * Create a note asynchronously.
@@ -13,7 +28,24 @@ const connectionString = process.env.DATABASE_URL;
  * @returns {Promise} Promise representing the object result of creating the note
  */
 async function create({ title, text, datetime } = {}) {
-  /* todo útfæra */
+  const client = new Client({ connectionString });
+  client.connect();
+  let res;
+  res = checkData(title, text, datetime);
+  if (res.length !== 0) {
+    return { status: 400, data: res };
+  }
+  try {
+    const query = 'INSERT INTO notes(datetime, title, text) VALUES($1, $2, $3) RETURNING *';
+    res = await client.query(query, [datetime, title, text].map(i => xss(i)));
+    if (res.rowCount === 0) return { status: 500, data: 'Internal error' };
+    return { status: 201, data: res.rows };
+  } catch (err) {
+    console.error('Error on inserting into data');
+    throw err;
+  } finally {
+    await client.end();
+  }
 }
 
 /**
@@ -22,7 +54,18 @@ async function create({ title, text, datetime } = {}) {
  * @returns {Promise} Promise representing an array of all note objects
  */
 async function readAll() {
-  /* todo útfæra */
+  const client = new Client({ connectionString });
+  client.connect();
+  let res;
+  try {
+    res = await client.query('SELECT * FROM notes');
+    return res.rows;
+  } catch (err) {
+    console.error('Error reading data');
+    throw err;
+  } finally {
+    await client.end();
+  }
 }
 
 /**
@@ -33,7 +76,21 @@ async function readAll() {
  * @returns {Promise} Promise representing the note object or null if not found
  */
 async function readOne(id) {
-  /* todo útfæra */
+  const client = new Client({ connectionString });
+
+  client.connect();
+
+  let res;
+  try {
+    res = await client.query('SELECT * FROM notes WHERE $1=id', [id]);
+    if (res.rowCount === 0) return { status: 404, data: { error: 'Note not found' } };
+    return { status: 200, data: res.rows };
+  } catch (err) {
+    console.error('Error reading from data');
+    throw err;
+  } finally {
+    await client.end();
+  }
 }
 
 /**
@@ -48,7 +105,26 @@ async function readOne(id) {
  * @returns {Promise} Promise representing the object result of creating the note
  */
 async function update(id, { title, text, datetime } = {}) {
-  /* todo útfæra */
+  const client = new Client({ connectionString });
+  client.connect();
+
+  let res;
+  const query = 'UPDATE notes SET title = $2, text = $3, datetime = $4 WHERE id = $1 RETURNING *';
+  res = await checkData(title, text, datetime);
+
+  if (res.length !== 0) {
+    return { status: 400, errors: res.errors };
+  }
+  try {
+    res = await client.query(query, [id, title, text, datetime].map(i => xss(i)));
+    if (res.rowCount === 0) return { status: 404, data: { error: 'Note not found' } };
+    return { status: 200, data: res.rows };
+  } catch (err) {
+    console.error('Error updating data');
+    throw err;
+  } finally {
+    await client.end();
+  }
 }
 
 /**
@@ -59,7 +135,22 @@ async function update(id, { title, text, datetime } = {}) {
  * @returns {Promise} Promise representing the boolean result of creating the note
  */
 async function del(id) {
-  /* todo útfæra */
+  const client = new Client({ connectionString });
+
+  client.connect();
+
+  let res;
+  try {
+    const query = 'DELETE FROM notes WHERE id = $1 RETURNING *';
+    res = await client.query(query, [id]);
+    if (res.rowsCount === 0) return { status: 404, data: { error: 'Note not found' } };
+    return { status: 204, data: res.rows };
+  } catch (err) {
+    console.error('Error deleting data');
+    throw err;
+  } finally {
+    await client.end();
+  }
 }
 
 module.exports = {
